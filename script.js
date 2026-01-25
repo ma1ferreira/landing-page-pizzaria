39ba9efcc46ff7f0666c03db5850ad2efaca3c18
ma1ferreira <mari.f3ira@gmail.com>
2026-01-24 22:23:46 -0300

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let show = true;
+const menuContent = document.querySelector('.content');
+const menuToggle = document.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+    menuContent.classList.toggle('on');
+});
